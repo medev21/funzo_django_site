9991ed2b234d13b05cb4a07cb6d79e495c58e30d
Martin Ben <mebenav88@gmail.com>
2016-02-13 16:35:57 -0500

    adding urls

---
+++ mysite/funzo/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from . import views
+
+# ^ beginning of string
+# $ end of string
+
+#url(what to look for, what function to grab, name of this url)
+
+urlpatterns = [
+    url(r'^$', views.index, name = 'index'), #this grabs the index from views.py
+]
