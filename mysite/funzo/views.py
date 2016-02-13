2d7ddc8af3778a36ccd02af384b4a49a7f3c3b7f
Martin Ben <mebenav88@gmail.com>
2016-02-13 17:17:44 -0500

    add more images

--- mysite/funzo/views.py
+++ mysite/funzo/views.py
@@ -7,11 +7,14 @@ import random
 def index(request):
     pic_url = random.choice((
         'https://tstoaddicts.files.wordpress.com/2013/11/funzo2.jpg',
-        'https://tstoaddicts.files.wordpress.com/2013/11/funzos-3.jpg'
+        'https://tstoaddicts.files.wordpress.com/2013/11/funzos-3.jpg',
+        'https://i.imgur.com/ECG486D.png',
+        'http://www.kvrx.org/sites/default/files/styles/user-image/public/staffphotos/funzo0sf.png.jpeg?itok=A7xl6f5V'
     ))
 
     context = {
         'pic_url' : pic_url,
+        'var1' : True,
     }
 
     return render(request, 'funzo/index.html', context)
